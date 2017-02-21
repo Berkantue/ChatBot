21af775b40abf5cad9e0ef773fa402e5472985cf
Berkantue <berkantuelger1@web.de>
2017-02-21 12:54:05 +0100

    Add files via upload

---
+++ Dialog mit Bot.py
@@ -0,0 +1,40 @@
+
+
+lesezeichen = 0
+import sys
+def funktion():
+        global lesezeichen
+        lesezeichen += 1
+        def schoener(s):
+                return input(">" + s +"\n* ")
+                print("Hallo ich bin Donald Trump,der Präsiedent von Amerika")
+                a = schoener("Wie heißt du ?: ")
+
+
+         if "Dann" in a:
+                schoener("Dann halt nicht !")
+                sys.exit()
+        
+
+         else:
+                 b = schoener("Wie gehts dir ?" + a)
+                 if "Juckt" in b:
+                schoener("Okey wie du meinst")
+
+         else :
+                d = schoener("Mir auch")
+
+                
+
+
+
+
+
+
+
+
+        
+        
+        
+        
+
---
+++ DonaldTrump.py
@@ -0,0 +1,28 @@
+
+
+
+
+name = "DonaldTrump"
+
+def sprich(zeile):
+	 
+
+	if ("understand" in zeile) or ("Hi" in zeile):
+		return"Hallo,Ich bin Donald Trump der Präsident von den Vereinigten Staaten von Amerika"
+	elif "Wie gehts dir" or "i don't understand" in zeile:
+		return"Mexikaner solten abgeschoben werden"
+	elif "JaJa" or "understand" or "i dont understand" in zeile:
+		return"Hillary Clinton ist ein pleb"
+	elif "Hi" in zeile:
+		return"Hi"
+	elif "dont understand" or "understand" in zeile :
+		return"Hulululu Ich bin Präsident von Amerika"
+	elif "see" in zeile:
+		return"Ich werde eine große Mauer bauen - und niemand baut Mauern besser als ich, glauben Sie mir - und ich baue sie sehr kostengünstig. Ich werde eine große, große Mauer an unserer südlichen Grenze bauen und ich werde Mexiko für diese Mauer bezahlen lassen"
+	elif "i see" in zeile:
+		return"Naja Kein Bock mehr BYe"
+	
+	return''
+
+""" kommentar """
+ 
